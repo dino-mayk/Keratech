43e06cb8c08e1ae936101ece77e4937a65c624a5
Nikita Krasnov <nikit0skrasnov@yandex.com>
2024-11-05 22:04:25 +0300

    robots.txt

--- Keratech/urls.py
+++ Keratech/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +10,14 @@ urlpatterns = [
     path('', include('homepage.urls'), name='homepage'),
     path('product/', include('product.urls'), name='product'),
     path('philosophy/', include('philosophy.urls'), name='philosophy'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="main/robots.txt",
+            content_type="text/plain",
+        )
+    ),
+
     # path('map/', include('map.urls'), name='map'),
 
     # path('ckeditor/', include('ckeditor_uploader.urls')),
